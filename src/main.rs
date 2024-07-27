bbd829195fe72824a545e745087d1426ea17bf68
Stefan Teunissen <stefanteunissen1@gmail.com>
2024-07-27 15:18:16 +0200

    Code refactor

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,10 @@
-use std::fs;
-use dialoguer::{theme::ColorfulTheme, FuzzySelect};
-use serde::Deserialize;
-use std::process::Command;
-use serde_json::Value;
-use walkdir::{DirEntry, WalkDir};
+use std::fs::File;
 use std::path::Path;
-
-fn clear_screen() {
-    println!("\x1B[2J\x1B[1;1H");
-}
+use std::process::Command;
+use dialoguer::{FuzzySelect, theme::ColorfulTheme};
+use serde::Deserialize;
+use serde_json::Value;
+use walkdir::{DirEntry, IntoIter, WalkDir};
 
 #[derive(Deserialize, Debug)]
 struct Server {
@@ -32,38 +28,39 @@ impl Server {
 }
 
 fn main() {
-    let root_environment_dir = "/Users/stefan-hypr/Tools/mage-db-sync-databases/";
+    let root_environment_dir: &str = "/Users/stefan-hypr/Tools/mage-db-sync-databases/";
 
-    let walker = WalkDir::new(root_environment_dir).into_iter();
-    let mut environment_selections = vec![];
+    let walker: IntoIter = WalkDir::new(root_environment_dir).into_iter();
+    let mut environment_selections: Vec<String> = vec![];
     for entry in walker {
-        let dir_entry = entry.unwrap().to_owned();
+        let dir_entry: DirEntry = entry.unwrap().to_owned();
         if !dir_entry.path().to_str().unwrap().ends_with(".json") { continue; }
 
         environment_selections.push(Path::new(dir_entry.path()).file_stem().unwrap().to_str().unwrap().to_owned());
     }
 
-    clear_screen();
+    //Clear screen and put cursor at the beginning
+    println!("\x1B[2J\x1B[1;1H");
 
-    let environment_selection = FuzzySelect::with_theme(&ColorfulTheme::default())
+    let environment_selection: usize = FuzzySelect::with_theme(&ColorfulTheme::default())
         .with_prompt("Select an environment")
         .default(0)
         .items(&environment_selections[..])
         .interact()
         .unwrap();
 
-    let selected_env = environment_selections[environment_selection].to_owned();
-    let environment_filename = format!("{root_environment_dir}{selected_env}.json");
+    let selected_env: String = environment_selections[environment_selection].to_owned();
+    let environment_filename: String = format!("{root_environment_dir}{selected_env}.json");
 
-    let file = fs::File::open(environment_filename)
+    let file: File = File::open(environment_filename)
         .expect("Unable to open file as read-only!");
-    let json: serde_json::Value = serde_json::from_reader(file)
+    let json: Value = serde_json::from_reader(file)
         .expect("File is invalid JSON!");
     let databases = json.get("databases")
         .expect("File should have a 'databases' key!");
 
     let mut stored_servers: Vec<Server> = vec![];
-    let mut server_selections = vec![];
+    let mut server_selections: Vec<String> = vec![];
     for database in databases.as_object().unwrap() {
         stored_servers.push(
             Server::new(
@@ -75,7 +72,7 @@ fn main() {
         server_selections.push(database.0.to_owned());
     }
 
-    let server_selection = FuzzySelect::with_theme(&ColorfulTheme::default())
+    let server_selection: usize = FuzzySelect::with_theme(&ColorfulTheme::default())
         .with_prompt("Select a server")
         .default(0)
         .max_length(10)
@@ -83,10 +80,10 @@ fn main() {
         .interact()
         .unwrap();
 
-    let username = stored_servers[server_selection].username.to_owned();
-    let server = stored_servers[server_selection].server.to_owned();
-    let port = stored_servers[server_selection].port.to_owned();
-    let username_server = format!("{username}@{server}");
+    let username: String = stored_servers[server_selection].username.to_owned();
+    let server: String = stored_servers[server_selection].server.to_owned();
+    let port: String = stored_servers[server_selection].port.to_owned();
+    let username_server: String = format!("{username}@{server}");
     // println!("{}", username_server);
     Command::new("ssh")
         .arg(username_server)
